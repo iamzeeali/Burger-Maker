58d5e36ec90d0c456032e8e68bbaef3c43b5e718
unknown <mdzeeshanali93@gmail.com>
2019-05-21 17:17:46 +0530

    Final Commit

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigatiomItem/NavigationItem";
+
+const navigationItems = props => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" exact>
+      Burger Maker
+    </NavigationItem>
+    {props.isAuthenticated ? (
+      <NavigationItem link="/orders">Orders</NavigationItem>
+    ) : null}
+    {!props.isAuthenticated ? (
+      <NavigationItem link="/auth">Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link="/logout">Logout</NavigationItem>
+    )}
+  </ul>
+);
+
+export default navigationItems;
---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,43 @@
+//BURGER BUILDER ACTION CREATORS
+import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
+
+export const addIngredient = name => {
+  return {
+    type: actionTypes.ADD_INGREDIENT,
+    ingredientName: name
+  };
+};
+
+export const removeIngredient = name => {
+  return {
+    type: actionTypes.REMOVE_INGREDIENT,
+    ingredientName: name
+  };
+};
+
+export const setIngredients = ingredients => {
+  return {
+    type: actionTypes.SET_INGREDIETNS,
+    ingredients: ingredients
+  };
+};
+
+export const fetchIngredientsFailed = () => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED
+  };
+};
+
+export const initIngredients = () => {
+  return dispatch => {
+    axios
+      .get("https://zee-react-my-burger.firebaseio.com/ingredients.json")
+      .then(response => {
+        dispatch(setIngredients(response.data));
+      })
+      .catch(error => {
+        dispatch(fetchIngredientsFailed(error));
+      });
+  };
+};
